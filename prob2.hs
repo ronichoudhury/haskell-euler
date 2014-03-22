78b94b2f499cb005f53826051151dbb9cc3c4c40
Roni Choudhury <roni@cs.utah.edu>
2014-03-21 19:30:04 -0500

    Simplified problem 2

--- prob2.hs
+++ prob2.hs
@@ -1,4 +1,4 @@
 fibo :: [Integer]
 fibo = 1:2:(zipWith (+) fibo (tail fibo))
 
-main = putStrLn . show . sum . takeWhile (<4000000) $ filter even fibo
+main = print . sum . takeWhile (<4000000) $ filter even fibo
