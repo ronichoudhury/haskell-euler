d868e6fd48748b3574e95b37073d99616fcb96b9
Roni Choudhury <roni@cs.utah.edu>
2014-03-21 19:33:51 -0500

    Simplified problem 4

--- prob4.hs
+++ prob4.hs
@@ -1,13 +1,14 @@
 import Data.List
 
-isPalindrome :: String -> Bool
-isPalindrome s = s == reverse s
+isPalindrome :: (Show a) => a -> Bool
+isPalindrome v = s == reverse s
+    where s = show v
 
 tripleDigitProducts :: [Integer]
-tripleDigitProducts = do a <- [999,998..100]
-                         b <- [999,998..100]
+tripleDigitProducts = do a <- [100..999]
+                         b <- [100..999]
                          return $ a * b
 
-palindromes = filter (isPalindrome . show) tripleDigitProducts
+palindromes = filter isPalindrome tripleDigitProducts
 
 main = print $ maximum palindromes
