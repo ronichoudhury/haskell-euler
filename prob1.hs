d829b9755840b1a23d0511c87c24fbccac031e30
Roni Choudhury <roni@cs.utah.edu>
2014-03-21 19:28:10 -0500

    Simplified problem 1

--- prob1.hs
+++ prob1.hs
@@ -1,12 +1,11 @@
 import Data.Functor
 
-countup :: [Integer]
-countup = 1 : (zipWith (+) (repeat 1) countup)
+ints = [1..]
 
 listSum = sum . takeWhile (<1000)
 
-threes = listSum $ (3*) <$> countup
-fives = listSum $ (5*) <$> countup
-fifteens = listSum $ (15*) <$> countup
+threes = listSum $ (3*) <$> ints
+fives = listSum $ (5*) <$> ints
+fifteens = listSum $ (15*) <$> ints
 
-main = putStrLn . show $ threes + fives - fifteens
+main = print $ threes + fives - fifteens
