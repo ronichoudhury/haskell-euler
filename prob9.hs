88dfedebc7337175896b21bab4038a522866e0a3
Roni Choudhury <roni@cs.utah.edu>
2014-03-25 19:39:30 -0500

    reporting product instead of triple in problem 9

--- prob9.hs
+++ prob9.hs
@@ -5,6 +5,9 @@ import Data.Maybe
 pythTriplet :: (Integral a) => (a, a, a) -> Bool
 pythTriplet (a,b,c) = a*a + b*b == c*c
 
+prodTriplet :: (Integral a) => (a, a, a) -> a
+prodTriplet (a,b,c) = a*b*c
+
 allTriples = map (\(a,b) -> (a, b, 1000 - a - b)) $ liftM2 (,) [1..998] [1..998]
 
-main = print . fromJust $ find pythTriplet allTriples
+main = print . prodTriplet . fromJust $ find pythTriplet allTriples
